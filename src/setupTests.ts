b36c21ff29c292265da1298e4034f2a3f9963d1f
mickey-ichi <mickey@react-plus.com>
2021-12-09 11:04:29 +0700

    clean

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,9 +2,4 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
-
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-
-import 'jest-styled-components';
+import '@testing-library/jest-dom';
