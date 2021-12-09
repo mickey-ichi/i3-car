5120afdfe31e1d95838451230272a0ba9704c286
macan28996 <canmt@reactplus.jp>
2021-12-09 16:32:32 +0700

    refactor button styled-components

--- src/app/components/Button/Button.stories.tsx
+++ src/app/components/Button/Button.stories.tsx
@@ -15,47 +15,9 @@ export default {
 
 export const Submit: ComponentStory<typeof Button> = () => (
   <div>
-    <Button types={'submit'} size={'xs'}>
-      Submit Small
-    </Button>
-    <p />
-    <Button types={'submit'} size={'md'}>
-      Submit Medium
-    </Button>
-    <p />
-    <Button types={'submit'} size={'lg'}>
-      Submit Large
-    </Button>
-  </div>
-);
-
-export const SubmitOutline: ComponentStory<typeof Button> = () => (
-  <div>
-    <Button types={'submitOutline'} size={'xs'}>
-      Submit Outline Small
-    </Button>
-    <p />
-    <Button types={'submitOutline'} size={'md'}>
-      Submit Outline Medium
-    </Button>
-    <p />
-    <Button types={'submitOutline'} size={'lg'}>
-      Submit Outline Large
-    </Button>
-  </div>
-);
-export const Cancel: ComponentStory<typeof Button> = () => (
-  <div>
-    <Button types={'cancel'} size={'xs'}>
-      Cancel Small
-    </Button>
-    <p />
-    <Button types={'cancel'} size={'md'}>
-      Cancel Medium
-    </Button>
-    <p />
-    <Button types={'cancel'} size={'lg'}>
-      Cancel Large
-    </Button>
+    <Button size={'xs'}>Submit Small</Button>
+    <Button size={'md'}>Submit Medium</Button>
+    <Button size={'lg'}>Submit Large</Button>
+    <Button>Submit Medium</Button>
   </div>
 );
--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -1,47 +1,24 @@
 import styled, { css } from 'styled-components';
 type ButtonType = {
-  types?: string;
-  size?: string;
+  size?: 'xs' | 'md' | 'lg';
+};
+const renderSize = (size = 'md') => {
+  if (size === 'xs') {
+    return css`
+      height: 32px;
+    `;
+  }
+  if (size === 'md') {
+    return css`
+      height: 40px;
+    `;
+  }
+  if (size === 'lg') {
+    return css`
+      height: 48px;
+    `;
+  }
 };
 export const Button = styled.button<ButtonType>`
-  width: 100%;
-  border-radius: 4px;
-  font-weight: 700;
-  font-size: 1rem;
-  border: transparent;
-  ${props =>
-    props.types === 'submit' &&
-    css`
-      background: #ff661a;
-      color: #ffffff;
-    `}
-  ${props =>
-    props.types === 'cancel' &&
-    css`
-      background: #f8f8f8;
-      border: 1px solid #cccccc;
-      color: #222222;
-    `}
- ${props =>
-    props.types === 'submitOutline' &&
-    css`
-      background: #ffffff;
-      border: 1px solid #f23d3d;
-      color: #f23d3d;
-    `}
- ${props =>
-    props.size === 'xs' &&
-    css`
-      height: 32px;
-    `}
-  ${props =>
-    props.size === 'md' &&
-    css`
-      height: 40px;
-    `}
-   ${props =>
-    props.size === 'lg' &&
-    css`
-      height: 48px;
-    `}
+  ${(props: ButtonType) => renderSize(props.size)}
 `;
